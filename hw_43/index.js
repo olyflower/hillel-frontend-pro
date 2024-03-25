c67d13c9f18e3254ee952aa2acb3e6a71286745b
olyflower <2013.olga.yudina@gmail.com>
2024-03-25 22:02:31 +0200

    added function

--- hw_43/index.js
+++ hw_43/index.js
@@ -1,19 +1,23 @@
+function changeInputs(inputs, textarea) {
+	let inputValues = [];
+
+	function checkValues() {
+		const newValues = inputs.map((input) => input.value);
+
+		if (JSON.stringify(inputValues) !== JSON.stringify(newValues)) {
+			inputValues = newValues;
+			textarea.value = inputValues.join(",");
+		}
+	}
+
+	checkValues();
+
+	setInterval(() => {
+		checkValues();
+	}, 3000);
+}
+
 const inputs = [...document.querySelectorAll("input")];
 const textarea = document.querySelector(".textarea");
 
-let inputValues = [];
-
-function checkValues() {
-	let newValues = inputs.map((input) => input.value);
-
-	if (JSON.stringify(inputValues) !== JSON.stringify(newValues)) {
-		inputValues = newValues;
-		textarea.value = inputValues.join(",");
-	}
-}
-
-checkValues();
-
-setInterval(() => {
-	checkValues();
-}, 3000);
+changeInputs(inputs, textarea);
