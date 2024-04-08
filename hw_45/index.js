1bc2922bba8c10846260e53ad5408ed576f379c4
olyflower <2013.olga.yudina@gmail.com>
2024-04-08 14:36:10 +0300

    remove for, add map, foreach

--- hw_45/index.js
+++ hw_45/index.js
@@ -1,16 +1,33 @@
-function getTable() {
-	const table = document.createElement("table");
-
-	for (let j = 0; j < 10; j++) {
-		const tr = document.createElement("tr");
-		table.append(tr);
-
-		for (let i = 1 + j * 10; i <= 10 + j * 10; i++) {
-			const td = document.createElement("td");
-			td.textContent = i;
-			tr.append(td);
-		}
-	}
-	return table
+function createTd(number) {
+	const td = document.createElement("td");
+	td.textContent = number;
+	return td;
 }
-document.body.append(getTable());
+
+function createTr(j) {
+	const tr = document.createElement("tr");
+	const numbers = Array.from(
+		{ length: 10 },
+		(_, index) => index + 1 + j * 10
+	);
+
+	numbers.map((item) => {
+		const td = createTd(item);
+		tr.append(td);
+	});
+	return tr;
+}
+
+function createTable() {
+	const table = document.createElement("table");
+	const numbers = Array.from({ length: 10 }, (_, index) => index);
+
+	numbers.map((item) => {
+		const tr = createTr(item);
+		table.append(tr);
+	});
+
+	return table;
+}
+
+document.body.append(createTable());
