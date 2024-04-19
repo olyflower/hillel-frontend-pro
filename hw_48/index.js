06283d4c317efab3707d60104b1e8229ed6e0c22
olyflower <2013.olga.yudina@gmail.com>
2024-04-19 11:18:55 +0300

    add changes

--- hw_48/index.js
+++ hw_48/index.js
@@ -1,50 +1,54 @@
-function onClickCell() {
-	const table = document.querySelector(".table");
+function createNode(tagName, textContent = "", classList) {
+	const node = document.createElement(tagName);
+	node.textContent = textContent;
+	if (classList) {
+		node.classList.add(classList);
+	}
+	return node;
+}
 
+function removeNode(node) {
+	node.remove();
+}
+
+function onClickCell(cell) {
+	const originalContent = cell.textContent;
+
+	if (cell.classList.contains("flag")) return;
+
+	const textarea = createNode("textarea", originalContent, "textarea");
+	const saveButton = createNode("button", "Save");
+	const cancelButton = createNode("button", "Cancel");
+
+	saveButton.addEventListener("click", function () {
+		cell.textContent = textarea.value;
+		removeNode(textarea);
+		removeNode(saveButton);
+		removeNode(cancelButton);
+		cell.classList.remove("flag");
+	});
+
+	cancelButton.addEventListener("click", function () {
+		cell.textContent = originalContent;
+		removeNode(textarea);
+		removeNode(saveButton);
+		removeNode(cancelButton);
+		cell.classList.remove("flag");
+	});
+
+	cell.textContent = null;
+	cell.classList.add("flag");
+	cell.append(textarea, saveButton, cancelButton);
+}
+
+function main() {
+	const table = document.querySelector(".table");
 	table.addEventListener("click", function (event) {
 		const target = event.target;
 
-		if (target.tagName === "TD" && !target.classList.contains("flag")) {
-			const fragment = document.createDocumentFragment();
-
-			const targetText = target.textContent;
-
-			const textarea = document.createElement("textarea");
-			textarea.value = targetText;
-			textarea.classList.add("textarea");
-
-			const buttonSave = document.createElement("button");
-			buttonSave.textContent = "Save";
-			buttonSave.addEventListener("click", function () {
-				target.textContent = textarea.value;
-				removeButtons();
-			});
-
-			const buttonCancel = document.createElement("button");
-			buttonCancel.textContent = "Cancel";
-			buttonCancel.addEventListener("click", function () {
-				target.textContent = targetText;
-				removeButtons();
-			});
-
-			function removeButtons() {
-				buttonCancel.remove();
-				buttonSave.remove();
-				textarea.remove();
-				target.classList.remove("flag");
-			}
-
-			target.classList.add("flag");
-
-			target.textContent = null;
-
-			fragment.append(textarea);
-			fragment.append(buttonSave);
-			fragment.append(buttonCancel);
-
-			target.append(fragment);
+		if (target.tagName === "TD") {
+			onClickCell(target);
 		}
 	});
 }
-
-onClickCell()
+main();
