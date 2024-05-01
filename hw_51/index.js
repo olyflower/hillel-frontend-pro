47a3789133bddcc6fc370f769660b0e5676eac8e
olyflower <2013.olga.yudina@gmail.com>
2024-05-01 21:47:27 +0300

    add changes

--- hw_51/index.js
+++ hw_51/index.js
@@ -10,17 +10,38 @@ function sendAjax({
 	const xhr = new XMLHttpRequest();
 
 	xhr.onload = () => {
+		if (xhr.status != 200) {
+			error(xhr.statusText);
+		}
 		data = JSON.parse(xhr.responseText);
 
-		elements.title.textContent = `Current weather in ${city}`;
-		elements.temp.textContent = `Temperature : ${data.main.temp} °C`;
-		elements.pressure.textContent = `Pressure ${data.main.pressure}`;
-		elements.description.textContent = `Description: ${data.weather[0].description}`;
-		elements.humidity.textContent = `Humidity: ${data.main.humidity} %`;
-		elements.speed.textContent = `Wind speed: ${data.wind.speed} m/s%`;
-		elements.deg.textContent = `Wind direction: ${data.wind.deg}`;
+		const {
+			main: { temp, pressure, humidity },
+			wind: { speed, deg },
+			weather: [{ description, icon }],
+		} = data;
 
-		elements.icon.src = `http://openweathermap.org/img/w/${data.weather[0].icon}.png`;
+		const {
+			title,
+			temp: tempElement,
+			pressure: pressureElement,
+			description: descriptionElement,
+			humidity: humidityElement,
+			speed: speedElement,
+			deg: degElement,
+			icon: iconElement,
+		} = elements;
+
+		title.textContent = `Current weather in ${city}`;
+		tempElement.textContent = `Temperature : ${temp} °C`;
+		pressureElement.textContent = `Pressure ${pressure}`;
+		descriptionElement.textContent = `Description: ${description}`;
+		humidityElement.textContent = `Humidity: ${humidity} %`;
+		speedElement.textContent = `Wind speed: ${speed} m/s`;
+		degElement.textContent = `Wind direction: ${deg}`;
+		iconElement.src = `http://openweathermap.org/img/w/${icon}.png`;
+
+		success(data);
 	};
 
 	xhr.onerror = error;
@@ -30,34 +51,49 @@ function sendAjax({
 	method === "GET" ? xhr.send() : xhr.send(data);
 }
 
-function selectElements() {
-	return {
-		title: document.querySelector(".weather__title"),
-		temp: document.querySelector(".weather__temp"),
-		pressure: document.querySelector(".weather__pressure"),
-		description: document.querySelector(".weather__description"),
-		humidity: document.querySelector(".weather__humidity"),
-		speed: document.querySelector(".weather__speed"),
-		deg: document.querySelector(".weather__deg"),
-		icon: document.querySelector(".weather__icon"),
+const getElements = () => {
+	let elements = null;
+	return () => {
+		if (!elements) {
+			elements = {
+				city: document.querySelector("#city"),
+				title: document.querySelector(".weather__title"),
+				temp: document.querySelector(".weather__temp"),
+				pressure: document.querySelector(".weather__pressure"),
+				description: document.querySelector(".weather__description"),
+				humidity: document.querySelector(".weather__humidity"),
+				speed: document.querySelector(".weather__speed"),
+				deg: document.querySelector(".weather__deg"),
+				icon: document.querySelector(".weather__icon"),
+			};
+		}
+		return elements;
 	};
-}
+};
+
+const getWetherOptions = getElements();
+
+const elements = getWetherOptions();
 
 function getWeather() {
 	document.querySelector(".button").addEventListener("click", function () {
-		const city = document.querySelector("#city").value;
-		if (city) {
-			const elements = selectElements();
-			sendAjax({
-				city: city,
-				url: `http://api.openweathermap.org/data/2.5/weather?q=${city}&units=metric&APPID=487bcb5fa64667c81d98bbdaed55ad5f`,
-				method: "GET",
-				elements,
-				error: (e) => {
-					console.log("Error", e);
-				},
-			});
+		const city = elements.city.value;
+		if (!city) {
+			return;
 		}
+
+		sendAjax({
+			city: city,
+			url: `http://api.openweathermap.org/data/2.5/weather?q=${city}&units=metric&APPID=487bcb5fa64667c81d98bbdaed55ad5f`,
+			method: "GET",
+			elements,
+			success: (data) => {
+				console.log(data, "");
+			},
+			error: (e) => {
+				console.log("Error", e);
+			},
+		});
 	});
 }
 
