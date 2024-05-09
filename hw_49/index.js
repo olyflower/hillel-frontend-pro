2a665a7a34140331abbae264374698a6309ca9bb
olyflower <2013.olga.yudina@gmail.com>
2024-05-10 00:42:02 +0300

    change function createForm

--- hw_49/index.js
+++ hw_49/index.js
@@ -31,6 +31,32 @@ const createForm = () => {
 		className: "form-order",
 	});
 
+	const contacts = createContactsFields();
+	const novaPoshta = createNPFields();
+	const paymentTypes = createPaymentTypes();
+
+	const button = createNode({
+		tagName: "button",
+		className: "button",
+		children: "Submit",
+	});
+
+	form.append(...contacts, ...novaPoshta, ...paymentTypes, button);
+
+	form.addEventListener("submit", function (event) {
+		const fullNameInput = form.querySelector(".form-order__fullname");
+		const novaPoshtaInput = form.querySelector(".form-order__novaposhta");
+
+		if (!fullNameInput.value || !novaPoshtaInput.value) {
+			event.preventDefault();
+			alert("Fill in required fields!");
+		}
+	});
+
+	return form;
+};
+
+const createContactsFields = () => {
 	const fullNameLabel = createNode({
 		tagName: "label",
 		className: "required",
@@ -61,36 +87,6 @@ const createForm = () => {
 		citySelect.append(option);
 	});
 
-	const novaPoshtaLabel = createNode({
-		tagName: "label",
-		className: "required",
-		children: "Enter Nova Poshta department:",
-	});
-
-	const novaPoshtaInput = createNode({
-		tagName: "input",
-		className: "form-order__novaposhta",
-	});
-
-	const paymentLabel = createNode({
-		tagName: "label",
-		children: "Select payment method:",
-	});
-
-	const paymentSelect = createNode({
-		tagName: "select",
-		className: "form-order__payment",
-	});
-
-	const payment = ["Card", "Cash"];
-	payment.forEach((item) => {
-		const option = createNode({
-			tagName: "option",
-			children: item,
-		});
-		paymentSelect.append(option);
-	});
-
 	const quantitySelectLabel = createNode({
 		tagName: "label",
 		children: "Select quantity:",
@@ -101,9 +97,7 @@ const createForm = () => {
 		className: "form-order__quantity",
 	});
 
-	const quantityItems = Array.from({ length: 100 }, (_, i) =>
-		(i + 1)
-	);
+	const quantityItems = Array.from({ length: 100 }, (_, i) => i + 1);
 
 	quantityItems.forEach((item) => {
 		const option = createNode({
@@ -123,36 +117,54 @@ const createForm = () => {
 		className: "form-order__comment",
 	});
 
-	const button = createNode({
-		tagName: "button",
-		className: "button",
-		children: "Submit",
-	});
-
-	form.append(
+	return [
 		fullNameLabel,
 		fullNameInput,
 		citySelectLabel,
 		citySelect,
-		novaPoshtaLabel,
-		novaPoshtaInput,
-		paymentLabel,
-		paymentSelect,
 		quantitySelectLabel,
 		quantitySelect,
 		textareaLabel,
 		textareaInput,
-		button
-	);
+	];
+};
 
-	form.addEventListener("submit", function (event) {
-		if (!fullNameInput.value || !NovaPoshtaInput.value) {
-			event.preventDefault();
-			alert("Fill in required fields!");
-		}
+const createNPFields = () => {
+	const novaPoshtaLabel = createNode({
+		tagName: "label",
+		className: "required",
+		children: "Enter Nova Poshta department:",
 	});
 
-	return form;
+	const novaPoshtaInput = createNode({
+		tagName: "input",
+		className: "form-order__novaposhta",
+	});
+
+	return [novaPoshtaLabel, novaPoshtaInput];
+};
+
+const createPaymentTypes = () => {
+	const paymentLabel = createNode({
+		tagName: "label",
+		children: "Select payment method:",
+	});
+
+	const paymentSelect = createNode({
+		tagName: "select",
+		className: "form-order__payment",
+	});
+
+	const payment = ["Card", "Cash"];
+	payment.forEach((item) => {
+		const option = createNode({
+			tagName: "option",
+			children: item,
+		});
+		paymentSelect.append(option);
+	});
+
+	return [paymentLabel, paymentSelect];
 };
 
 //Order logic
